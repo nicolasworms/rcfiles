ea6d27cc90787a54c211c7a0658dbbe095cf95e3
nicolasworms <n.nicolas.clement@gmail.com>
2016-12-15 17:20:39 +0100

    [setup] including pip and ipython

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,6 +1,6 @@
 sudo apt-get install aptitude &&
 sudo aptitude install vim &&
-sudo aptitude install pip &&
+sudo aptitude install python-pip &&
 sudo aptitude install ipython &&
 sudo aptitude update &&
 sudo aptitude upgrade
